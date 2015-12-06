3ac845da17fa9cc7fe02aef87c355f61e8cf5ef8
Antti Risteli <antti@kiinnost.us>
2015-12-06 21:45:04 +0200

    Switch to internal pull up instead of external pull down

--- arduino_softa/arduino_softa.ino
+++ arduino_softa/arduino_softa.ino
@@ -13,7 +13,7 @@ void setup() {
   pinMode(RELAY_POWER_PORT, OUTPUT);
   pinMode(RELAY_BRIDGE_1_PORT, OUTPUT);
   pinMode(RELAY_BRIDGE_2_PORT, OUTPUT);
-  pinMode(PULSE_TRIGGER_PORT, INPUT);
+  pinMode(PULSE_TRIGGER_PORT, INPUT_PULLUP);
   // set all the relays to known state
   digitalWrite(RELAY_POWER_PORT, LOW);
   lastWasLow = 1;
@@ -41,7 +41,7 @@ void loop() {
   if (lastMinute != minute()) {
     lastMinute = minute();
     sendSwitchPulse();
-  } else if (digitalRead(PULSE_TRIGGER_PORT) == HIGH) {
+  } else if (digitalRead(PULSE_TRIGGER_PORT) == LOW) {
     sendSwitchPulse();
   }
 }
