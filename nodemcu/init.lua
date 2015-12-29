786f784b74d85ce480ada9b2876e892f5edb48d3
Antti Risteli <antti@kiinnost.us>
2015-12-29 23:27:51 +0200

    Add local time keeping to nodemcu version

--- nodemcu/init.lua
+++ nodemcu/init.lua
@@ -1,3 +1,16 @@
+-- the timing logic operates in two modes:
+-- 1. fallback mode, where a chip timer is used (tmr.now())
+-- 2. synced mode, where the sntp is used to sync the time
+--
+-- when booted, the mode will first be fallback. the logic pretty much
+-- repeats calculating tmr.now() / 60 000 000. overflows are also handled
+--
+-- once sntp connection has been established once, the mode is changed.
+-- initial sntp time fix is stored, and later (rtctime.get() - initial) / 60
+-- is calculated for changes
+
+MICROSECONDS_IN_MINUTE = 60000000
+
 TIMER_POLL_TIME_AND_SWITCH = 0
 TIMER_PULSE_CONTROL = 1
 
@@ -7,6 +20,7 @@ PIN_H_BRIDGE_B = 7
 PIN_INPUT_SWITCH = 0
 
 last_was_low = false
+has_time_synced = false
 
 -- init
 gpio.mode(PIN_H_BRIDGE_ENABLE, gpio.OUTPUT)
@@ -38,10 +52,37 @@ function send_pulse()
   end)
 end
 
+-- time utils
+
+tmr_now_overflow_count = 0
+tmr_now_last = 0
+function tmr_now_with_overflows()
+  local now = tmr.now()
+  if (now < tmr_now_last) then
+    tmr_now_overflow_count = tmr_now_overflow_count + 1
+  end
+  return tmr_now_overflow_count * (2^31 - 1) + now
+end
+
+last_minute_count = 0
+function minute_has_changed()
+  if (not has_time_synced) then
+    local new_minute_count = math.floor(tmr_now_with_overflows() / MICROSECONDS_IN_MINUTE)
+    if (last_minute_count ~= new_minute_count) then
+      print("minute has changed")
+      last_minute_count = new_minute_count
+      return true
+    end
+    return false
+  else
+    return false
+  end
+end
+
 -- time and switch polling
 
 tmr.alarm(TIMER_POLL_TIME_AND_SWITCH, 100, 1, function() 
-  if (gpio.read(PIN_INPUT_SWITCH) == gpio.LOW) then
+  if (gpio.read(PIN_INPUT_SWITCH) == gpio.LOW or minute_has_changed()) then
     send_pulse()
   end
 end)
